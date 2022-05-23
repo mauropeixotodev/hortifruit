7d9327f518552aef436386e401a7e714301599c5
Mauro Peixoto <maurograp24@gmail.com>
2022-05-23 16:09:50 -0300

    Primeiro commit projeto hortifruit

---
+++ src/main/java/com/example/hortfruitonline/HortfruitOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.hortfruitonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortfruitOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HortfruitOnlineApplication.class, args);
+	}
+
+}
